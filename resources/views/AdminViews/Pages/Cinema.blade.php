4348c827af3be58b5f9963ea558ee100debe8d0d
datthudo <nguyendat31012003@gmail.com>
2023-09-23 20:04:30 +0700

    up date thêm, sửa, xóa bảng CInema

---
+++ resources/views/AdminViews/Pages/Cinema.blade.php
@@ -0,0 +1,55 @@
+<div class="page-wrapper dashboard-wrap">
+    <div class="content container-fluid">
+  <div class="container-xl">
+    <div class="table-responsive">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-8">
+                        <h2>Cinema <b>Details</b>       <button class="btn btn-primary ">
+                        <a href="./cinema/create" style="color: white">Add new Cinema</a>
+                        </button></h2>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="search-box">
+                            <form action="#">
+                                <i class="material-icons">&#xE8B6;</i>
+                                <input type="text" class="form-control" placeholder="Search&hellip;">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-striped table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name <i class="fa fa-sort"></i></th>
+                        <th>Địa chỉ</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($danhsach as $Cinema)
+                 <tr>
+                     <td>{{$Cinema->Id}}</td>
+                     <td>{{$Cinema->Name}}</td>
+                     <td>{{$Cinema->Address}}</td>
+                     <td>
+                        <a href="admin/cinema/{{$Cinema->Id}}" class="view" title="View" data-toggle="tooltip"><i class="material-icons">&#xE417;</i></a>
+                        <a href="admin/cinema/{{$Cinema->Id}}/edit" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
+                        <a href="/admin/cinema/delete/{{$Cinema->Id}}" onclick="return confirm('Your sure delete cinema has name: {{$Cinema->Name}}')" class="detele?id=" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
+                    </td>
+  
+                 </tr>
+                    @endforeach
+                </tbody>
+            </table>
+  
+        </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  
+  
---
+++ resources/views/AdminViews/Pages/CinemaEdit.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="http://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Edit Cinema </title>
+</head>
+<>
+    <div class="page-wrapper dashboard-wrap">
+        <div class="content container-fluid">
+            <div class="container">
+                <form method="post" action="/admin/director{{$Director->Id}}">
+                    <h1>Edit Cinema</h1>
+                    @csrf
+                    @method('PUT')
+                    <div class="form-group">
+                        <label for="nameInput">Name</label>
+                        <input type="text" class="form-control" id="nameInput" name="Name" value="{{$Director->Name}}" placeholder="Enter Name">
+                    </div>
+<button type="submit" class="btn btn-primary">Submit </button>
+                </form>
+
+            </div>
+
+        </div>
+    </div>
+    <script src="http://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
